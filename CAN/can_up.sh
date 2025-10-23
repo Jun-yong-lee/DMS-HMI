daf209e5ef4d39756853c4131274f3445ddcb517
junyong lee <71388422+Jun-yong-lee@users.noreply.github.com>
2025-10-23 20:49:32 +0900

    Add | M-CAN can-bus settings

--- CAN/can_up.sh
+++ CAN/can_up.sh
@@ -1,5 +1,7 @@
 sudo modprobe can
 sudo modprobe kvaser_usb
-sudo ip link set can0 type can bitrate 500000
+sudo ip link set can0 type can bitrate 500000 # C-CAN
+sudo ip link set can2 type can bitrate 100000 # M-CAN
 sudo ifconfig can0 up
-# workon dmd
+sudo ifconfig can2 up
+# workon dms-hmi
